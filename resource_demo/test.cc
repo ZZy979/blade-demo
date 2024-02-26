b4cdea9f971a3d50fb6d333245258befbec65c64
ZZy979 <979481894@qq.com>
2024-02-26 21:36:02 +0800

    增加resource_library示例

---
+++ resource_demo/test.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+#include "resource_demo/foo.h"
+
+int main() {
+    std::string a(RESOURCE_resource_demo_a_txt, RESOURCE_resource_demo_a_txt_len);
+    std::string b(RESOURCE_resource_demo_b_txt, RESOURCE_resource_demo_b_txt_len);
+    std::cout << a << '\n' << b << '\n';
+    return 0;
+}
